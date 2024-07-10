bd36b500d796387c3d02234939ba1f9c006e038f
Elton Storari <eltonstorari@gmail.com>
2024-07-10 17:03:10 -0300

    Metodo para saber quanto tempo a lasanha deve ficar no forno

--- java/lasanha/src/lasanha/Main.java
+++ java/lasanha/src/lasanha/Main.java
@@ -3,8 +3,20 @@ package lasanha;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// Criando instancia dos metodos da lasagna 
+		Lasagna lasagna = new Lasagna();
+		
+		System.out.println("Tempo esperado no forno: " + lasagna.expectedMinutesInOven() + " Minutos");
+		
+	}
+	
+	
+	public static class Lasagna {
 
+        public int expectedMinutesInOven(){
+            return 40;
+        }
 	}
 
 }
