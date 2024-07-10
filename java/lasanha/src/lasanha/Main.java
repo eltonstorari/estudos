b15cad40d981bea5fe40144597d9fac7401ad5f0
Elton Storari <eltonstorari@gmail.com>
2024-07-10 17:26:58 -0300

    Metodo que usa a quantidade de camada para retornar o tempo de a lasanha sera preparada

--- java/lasanha/src/lasanha/Main.java
+++ java/lasanha/src/lasanha/Main.java
@@ -12,6 +12,9 @@ public class Main {
 		// Paramentro de entrada é referente o tempo que a lasanha Já ficou no forno. 10 minutos
 		System.out.println("Tempo que a lasanha ainda precisa ficar no forno: " + lasagna.remainingMinutesInOven(10) + " Minutos");
 		
+		// paramento de entrada e quantas camadas é preciso para fazer a lasanha e retorna o tempo de preparo 20 * 2 = 40 min
+		System.out.println("A lasanha ficará pronta em: " + lasagna.preparationTimeInMinutes(20));
+		
 	}
 	
 	
@@ -23,6 +26,10 @@ public class Main {
         
         public int remainingMinutesInOven(int permaneceuNoFogo){
             return expectedMinutesInOven() - permaneceuNoFogo;
+        }
+        
+        public int preparationTimeInMinutes(int qtdeCamadas){
+            return qtdeCamadas * 2;
         }
 	}
 
