623aa252b0809831abb996623bacf4d485e643f8
Elton Storari <eltonstorari@gmail.com>
2024-07-10 17:18:58 -0300

    Metodo que retorna quanto tempo a lasanha AINDA deve ficar no forno de acordo com o parametro de entrada

--- java/lasanha/src/lasanha/Main.java
+++ java/lasanha/src/lasanha/Main.java
@@ -9,6 +9,9 @@ public class Main {
 		
 		System.out.println("Tempo esperado no forno: " + lasagna.expectedMinutesInOven() + " Minutos");
 		
+		// Paramentro de entrada é referente o tempo que a lasanha Já ficou no forno. 10 minutos
+		System.out.println("Tempo que a lasanha ainda precisa ficar no forno: " + lasagna.remainingMinutesInOven(10) + " Minutos");
+		
 	}
 	
 	
@@ -16,6 +19,10 @@ public class Main {
 
         public int expectedMinutesInOven(){
             return 40;
+        }
+        
+        public int remainingMinutesInOven(int permaneceuNoFogo){
+            return expectedMinutesInOven() - permaneceuNoFogo;
         }
 	}
 
