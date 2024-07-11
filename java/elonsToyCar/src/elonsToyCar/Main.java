eec41b61f7953e94d638bfe430a4416e093949bc
Elton Storari <eltonstorari@gmail.com>
2024-07-10 22:52:51 -0300

    Adicionei o display do carrinho

--- java/elonsToyCar/src/elonsToyCar/Main.java
+++ java/elonsToyCar/src/elonsToyCar/Main.java
@@ -3,15 +3,24 @@ package elonsToyCar;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		ElonsToyCar elonsToyCar = new ElonsToyCar();
+		
+		System.out.println(elonsToyCar.distanceDisplay());
 
 	}
 
 	public static class ElonsToyCar {
+		private int distance = 0;
 		
 	    public static ElonsToyCar buy() {
 	        ElonsToyCar car = new ElonsToyCar();
 	        return car;
+	    }
+	    
+	    public String distanceDisplay() {
+	        return "Driven " + distance + " meters";
+	    
 	    }
 		
 	}
